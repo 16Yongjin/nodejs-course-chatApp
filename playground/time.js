237a67ca65b8138653e11e1e14202b788b4bd6be
16Yongjin <yongjin0802@gmail.com>
2017-06-27 17:12:16 +0900

    add moment

---
+++ playground/time.js
@@ -0,0 +1,10 @@
+const moment = require('moment');
+
+// var date = moment();
+// console.log(date.format('MMM Do YYYY, HH:mm:ss a'));
+
+// console.log(date.format('h:m a'));
+// console.log(date.subtract(4, 'hours').add(10, 'm').format('h:mm a'));
+
+var someTimestamp = moment().valueOf();
+console.log(someTimestamp);
